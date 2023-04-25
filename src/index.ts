189aa0b77df7d53f51e8cd85cc27adea7a4aed69
Martin Patino <martin@sibipro.com>
2023-04-24 17:44:18 -0700

    feat: Update project to be in typescript
    Just switching up project to be in nodejs

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import figlet from "figlet";
+import { OpenAI } from "langchain/llms/openai";
+
+
+async function main() {
+  const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9 });
+
+  const res = await model.call(
+    "What would be a good company name a company that makes colorful socks?"
+  );
+
+  console.log(res);
+}
+
+
+main()
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// vite.config.js
+const vite_1 = require("vite");
+exports.default = (0, vite_1.defineConfig)({
+  plugins: [],
+});
+//# sourceMappingURL=vite.config.js.map
