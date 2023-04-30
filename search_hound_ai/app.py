73cad3a37159709e7a43c1d4017cc5bfb293ddb8
thisguymartin <mpatino117@gmail.com>
2023-04-30 13:32:06 -0700

    chore: update 0.1.11

--- search_hound_ai/app.py
+++ search_hound_ai/app.py
@@ -2,11 +2,16 @@ import search_hound_ai
 import typer
 
 from dotenv import load_dotenv
-from validate_env import validate_env
-
 from hound_client_commands import HoundClientCommands
 
 load_dotenv()
+
+def validate_env():
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    if OPENAI_API_KEY == None or OPENAI_API_KEY == '':
+        print("OPENAI_API_KEY Environment Variable is not set.")
+        exit(1)
+
 
 def main():
     app = typer.Typer(name=search_hound_ai.__app_name__, add_completion=True)
@@ -17,4 +22,5 @@ def main():
 if __name__ == "__main__":    
     validate_env()
     main()
+
 
--- search_hound_ai/validate_env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def validate_env():
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    if OPENAI_API_KEY == None or OPENAI_API_KEY == '':
-        print("OPENAI_API_KEY Environment Variable is not set.")
-        exit(1)
