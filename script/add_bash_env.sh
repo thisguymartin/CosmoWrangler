c3d740f16023fcec68802d072ea96d0638af2373
thisguymartin <mpatino117@gmail.com>
2023-04-30 14:04:44 -0700

    chore: update docs

--- script/add_bash_env.sh
+++ script/add_bash_env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Prompt user for environment variable value
-read -p "Enter value for OPENAI_API_KEY: " env_value
+echo -n "Enter value for OPENAI_API_KEY: "
+read env_value
 
 # Add environment variable to .bashrc file
-echo "export $env_var=$env_value" >> ~/.bashrc
+echo "export OPENAI_API_KEY=$env_value" >> ~/.bashrc
 
 # Reload .bashrc file
 source ~/.bashrc
 
 # Print message to console
-echo "The $env_var environment variable has been added to ~/.bashrc with value $env_value."
+echo "The OPENAI_API_KEY environment variable has been added to ~/.bashrc with value $env_value."
